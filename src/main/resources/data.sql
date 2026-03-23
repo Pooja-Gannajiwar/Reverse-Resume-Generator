2db9db487a8b26f338f1991d35a599d8e32ced3e
Pooja Gannajiwar <23eg105t21@anurag.edu.in>
2026-03-23 21:06:15 +0530

    Fix production CORS

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO jobs (title, company, location, description)
+VALUES
+('Java Developer', 'Infosys', 'Hyderabad', 'Spring Boot, REST APIs'),
+('Backend Engineer', 'TCS', 'Bangalore', 'Java, Microservices'),
+('Software Engineer', 'Wipro', 'Pune', 'Java, SQL');
